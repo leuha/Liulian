f056cec9b4badcd41629296cd9302a475d8f2ebf
raytheon429 <raytheon429@aliyun.com>
2016-08-11 21:31:11 +0800

    添加注册页面中……

---
+++ app/src/main/java/com/helloworld/liulian/bean/User.java
@@ -0,0 +1,10 @@
+package com.helloworld.liulian.bean;
+
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Created by Administrator on 2016/8/9.
+ */
+public class User extends BmobUser{
+    Integer AMapId;
+}
